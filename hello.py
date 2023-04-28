7ca59b4da1970601d5af36b7cb07a28f6aca4dfa
adithya <adithya.k@sacumen.com>
2023-04-28 17:10:07 +0530

    rebased for 3rd time

--- hello.py
+++ hello.py
@@ -1,2 +1,22 @@
 def greet():
     print("Hello From Sourabh")
+
+def greet_p():
+    print("Hi pragati this side")
+
+# creating pattern 
+
+# taking int input from user
+num=int(input("enter number:"))
+
+# looping num
+for row in range(num):
+    for col in range(num):
+        if row+col<=num-1:
+            print("*",end=" ")
+        else:
+            print(" ",end=" ")
+    print()
+
+def my_hello():
+    print("Hellooo... from my side.")
